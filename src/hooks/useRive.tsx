b7cf5ede3ab62f6f78460da84a58ea245db7e81d
Zach Plata <plata.zach@gmail.com>
2022-03-14 15:39:15 -0700

    testing with webgl and new npm package for testing

--- src/hooks/useRive.tsx
+++ src/hooks/useRive.tsx
@@ -6,7 +6,7 @@ import React, {
   ComponentProps,
   RefCallback,
 } from 'react';
-import { Rive, EventType } from '@rive-app/canvas';
+import { Rive, EventType } from '@rive-app/webgl';
 import {
   UseRiveParameters,
   UseRiveOptions,
