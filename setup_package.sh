f364ed56623e4cef307372edfd55fde5f2def9a4
Zach Plata <plata.zach@gmail.com>
2022-03-16 00:00:40 -0700

    some more tweaks

--- setup_package.sh
+++ setup_package.sh
@@ -3,5 +3,4 @@ set -e
 
 # Setup the package.json for a given npm module
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-# REMOVE: ONLY FOR FIRST PUBLISH USE
-node $SCRIPT_DIR/trimPackageJson.js `pwd` "${BASH_SOURCE[0]}"
+node $SCRIPT_DIR/trimPackageJson.js `pwd` "$1"
